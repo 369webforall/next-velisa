3858988f952708c1eeba7fe4063dc488e55a44a5
369webforall <369webforall@gmail.com>
2024-08-22 14:34:48 +0545

    new feature

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,22 @@ const HomePage = () => {
       <h1 className="text-3xl text-center mt-10">
         This is home page hosted in github
       </h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia quae
+        assumenda consequuntur explicabo doloribus atque, voluptas optio
+        adipisci officiis distinctio odio omnis ratione iste, sapiente totam
+        ducimus unde quibusdam maiores.
+      </p>
+      <h2>Name list</h2>
+      <ul>
+        <li>One</li>
+        <li>One</li>
+        <li>One</li>
+        <li>One</li>
+        <li>One</li>
+        <li>One</li>
+        <li>One</li>
+      </ul>
     </div>
   );
 };
